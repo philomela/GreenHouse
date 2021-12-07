e302d2b10ce5a80a00c015044ac6ed87f374f8e6
philomela <romaphilomela@yandex.ru>
2021-12-08 02:51:13 +0300

    greenhouse-01. Added new projects

---
+++ greenhouse_app/Interfaces/IControlable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace greenhouse_app.Interfaces
+{
+    public interface IControlable
+    {
+        void TurnOnLamps();
+
+        void TurnOffLamps();
+
+        void TurnOnFans();
+
+        void TurnOffFans();
+
+        void TurnOnWatering();
+
+        void TurnOffWatering();
+    }
+}
+
